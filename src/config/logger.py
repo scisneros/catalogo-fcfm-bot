22615a18fe87dbcd485fc1556e52609e7cd85509
scisneros <sebastian.cisneros384@gmail.com>
2019-07-11 18:44:42 -0400

    Set log max size and rotation + minor changes in log lines

--- src/config/logger.py
+++ src/config/logger.py
@@ -1,6 +1,15 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(filename='../bot.log',
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s - [%(funcName)s:%(lineno)d]')
+
+logFile = '../bot.log'
+
+# 3 MB max files, up to 2 backup files.
+my_handler = RotatingFileHandler(logFile, mode='a', maxBytes=3*1024*1024, backupCount=2, encoding=None, delay=0)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+
 logger = logging.getLogger('CatalogoFCFMBot')
+logger.setLevel(logging.INFO)
+logger.addHandler(my_handler)
