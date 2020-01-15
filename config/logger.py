62a03c7c68fa572311dfac59b3f3bd7d3f605fd4
scisneros <sebastian.cisneros384@gmail.com>
2020-01-15 00:24:41 -0300

    Log to stdout too

--- config/logger.py
+++ config/logger.py
@@ -1,11 +1,14 @@
 import logging
+import sys
 from logging.handlers import RotatingFileHandler
+from logging import StreamHandler
 from os import path
 
 # 3 MB max files, up to 2 backup files.
 logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s - [%(funcName)s:%(lineno)d]',
                     level=logging.INFO,
                     handlers=[RotatingFileHandler(path.relpath('bot.log'), mode='a', maxBytes=3*1024*1024,
-                                                  backupCount=2, encoding=None, delay=0)])
+                                                  backupCount=5, encoding=None, delay=0),
+                              StreamHandler(sys.stdout)])
 
 logger = logging.getLogger('CatalogoFCFMBot')
